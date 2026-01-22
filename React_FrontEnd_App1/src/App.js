574275076b130441b4e1a3005701b7bf479e0d58
aezazkhatri09-spec <aezazkhatri09@gmail.com>
2026-01-22 15:17:49 +0530

    Create App.js

---
+++ React_FrontEnd_App1/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import NavbarSection from '../src/Components/NavbarSection';
+import MainContent from '../src/Components/MainContent';
+import FooterSection from '../src/Components/FooterSection';
+
+function App() {
+  return (
+    <>
+      <NavbarSection />
+      <MainContent />
+      <FooterSection />
+    </>
+  );
+}
+
+export default App;
